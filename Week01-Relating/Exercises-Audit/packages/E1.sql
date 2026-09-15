b0c217be4e99122319632836549ece6d6045ba50
EmptinessOutside <criscordova_04@hotmail.com>
2026-09-14 22:35:03 -0500

    feat: some fixes in files

--- Week01-Relating/Exercises-Audit/packages/E1.sql
+++
@@ -1,7 +0,0 @@
---SELECT * FROM "addresses"
---WHERE "address" = '900 Somerville Avenue'; 
---432
-
-SELECT * FROM "packages"
-JOIN "addresses" ON "addresses"."id" = "packages"."to_address_id"
-WHERE "from_address_id" = 432 AND "contents" = 'Congratulatory letter';
--- Week01-Relating/Exercises-Audit/packages/log.sql
+++ Week01-Relating/Exercises-Audit/packages/log.sql
@@ -1,6 +1,8 @@
 
 -- *** The Lost Letter ***
-
+SELECT "address", "type" FROM "packages"
+JOIN "addresses" ON "addresses"."id" = "packages"."to_address_id"
+WHERE "from_address_id" = 432 AND "contents" = 'Congratulatory letter';
 -- *** The Devious Delivery ***
 
 -- *** The Forgotten Gift ***
